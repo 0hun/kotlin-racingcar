db4dc07c272d0206863ef8a59aa9d7f20028d619
younghun12 <120432704+younghun12@users.noreply.github.com>
2024-01-22 21:16:47 +0900

    feat : Car class 추가

---
+++ src/main/kotlin/domain/Car.kt
@@ -0,0 +1,18 @@
+package domain
+
+class Car(name: String, movableStrategy: MovableStrategy?) {
+
+    val name: String = name
+    var position: Int = 0
+    var movableStrategy: MovableStrategy? = movableStrategy
+
+    fun move() {
+        if (movableStrategy!!.isMovable()) {
+            this.position += 1
+        }
+    }
+}
+
+fun interface MovableStrategy {
+    fun isMovable(): Boolean
+}
