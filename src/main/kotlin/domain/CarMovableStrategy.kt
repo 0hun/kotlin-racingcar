b6b49ca79c6edb596e80173b0e7b52e93032c008
younghun12 <120432704+younghun12@users.noreply.github.com>
2024-01-23 20:21:18 +0900

    refactor : CarMovableStrategy에서 generateRandomNumber() 함수 사용하도록 수정

--- src/main/kotlin/domain/CarMovableStrategy.kt
+++ src/main/kotlin/domain/CarMovableStrategy.kt
@@ -1,26 +1,17 @@
 package domain
 
-import java.util.*
+import util.generateRandomNumber
+
+
+private const val MOVE_CONDITION_NUM = 4
 
 class CarMovableStrategy : MovableStrategy {
     override fun isMovable(): Boolean {
-        return RandomNumber.generator() >= MOVE_CONDITION_NUM
+        return generateRandomNumber() >= MOVE_CONDITION_NUM
     }
 
-    companion object {
-        private const val MOVE_CONDITION_NUM = 4
-    }
 }
 
 fun interface MovableStrategy {
     fun isMovable(): Boolean
-}
-
-object RandomNumber {
-    private const val RANDOM_SIZE = 10
-
-    fun generator(): Int {
-        val random = Random()
-        return random.nextInt(RANDOM_SIZE)
-    }
 }
